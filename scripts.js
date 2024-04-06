5658f349ef6b18dc8c32a7189ae1fc6890996d19
andersondbk <32954765+andersondbk@users.noreply.github.com>
2024-04-06 16:22:33 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,8 @@
-function enlargeImage(imgId) {
-    const img = document.getElementById(imgId);
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.innerHTML = `<span class="close">&times;</span><img class="modal-content" id="modal-${imgId}">`;
-    document.body.appendChild(modal);
-
-    const modalImg = document.getElementById(`modal-${imgId}`);
-    modalImg.src = img.src;
-    modal.style.display = "block";
-
-    const span = modal.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-        modal.remove(); // Clean up by removing the modal from the DOM after closing
-    }
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
